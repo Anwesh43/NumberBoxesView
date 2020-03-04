9244d7d34622fe2ac767fb7d6e711f15eb37a48c
Anwesh43 <anweshthecool0@gmail.com>
2020-03-04 11:22:39 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/numberboxesview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/numberboxesview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.numberboxesview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.nbview.NumberBoxesView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        NumberBoxesView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
