170ee4e45fb8cd1882f40d44f875014c5268d6a9
Anwesh43 <anweshthecool0@gmail.com>
2020-03-04 10:34:45 +0530

    adding libraries and constants

--- nbview/src/main/java/com/anwesh/uiprojects/nbview/NumberBoxesView.kt
+++ nbview/src/main/java/com/anwesh/uiprojects/nbview/NumberBoxesView.kt
@@ -3,3 +3,18 @@ package com.anwesh.uiprojects.nbview
 /**
  * Created by anweshmishra on 04/03/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val boxes : Int = 6
+val scGap : Float = 0.02f
+val boxColor : Int = Color.parseColor("#4CAF50")
+val textColor : Int = Color.parseColor("#FFFFFF")
+val sizeFactor : Float = 5.8f
