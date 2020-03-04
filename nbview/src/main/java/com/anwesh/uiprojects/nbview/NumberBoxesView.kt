3e62c94737b434b4f7b824fcaa4b649757d6988a
Anwesh43 <anweshthecool0@gmail.com>
2020-03-04 10:41:53 +0530

    adding NumberBoxesView class

--- nbview/src/main/java/com/anwesh/uiprojects/nbview/NumberBoxesView.kt
+++ nbview/src/main/java/com/anwesh/uiprojects/nbview/NumberBoxesView.kt
@@ -36,3 +36,20 @@ fun Canvas.drawNumberBox(i : Int, scale : Float, paint : Paint) {
     restore()
 }
 
+class NumberBoxesView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
